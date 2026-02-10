a25ad45f5f6e05faa60e2bb12b6a708d381825b0
JamilurRahmanKhan <86043227+JamilurRahmanKhan@users.noreply.github.com>
2026-02-10 12:43:00 +0600

    chore: set up eslint and prettier (#4)

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+const tseslint = require("typescript-eslint");
+
+module.exports = tseslint.config(
+  { ignores: ["dist", "node_modules", "coverage"] },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module"
+    },
+    plugins: {
+      // typescript-eslint provides recommended configs + rules
+    },
+    extends: [
+      ...tseslint.configs.recommended,
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:import/recommended",
+      "prettier"
+    ],
+    settings: {
+      react: { version: "detect" }
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }]
+    }
+  }
+);
